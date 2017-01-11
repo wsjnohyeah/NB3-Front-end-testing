e66044cbf8d34f79e1f09f7042a5a03b02136788
Ethan Hu <ethan.hqc@icloud.com>
2017-01-12 03:16:27 +0800

    test auto-pull

--- public/deploy.php
+++ public/deploy.php
@@ -3,7 +3,6 @@ echo "running on".shell_exec("whoami");
 
 $www_folder = "/var/www/html/testing/" ;
 
-$git_repo = "git@github.com:wsjnohyeah/laravel-testing.git" ;
-
 //执行指令 
-echo shell_exec(" cd $www_folder && git pull $git_repo 2>&1 ");
+echo shell_exec("cd ".$www_folder);
+echo shell_exec("git pull origin master");
