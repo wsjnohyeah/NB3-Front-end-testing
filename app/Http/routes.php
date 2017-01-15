767e6b3cc637bd542e2aebfcef18a286b062e57e
Ethan Hu <ethan.hqc@icloud.com>
2017-01-16 00:38:41 +0800

    na

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,8 +11,8 @@
 |
 */
 
-Route::get('/', 'pages\PagesController@index');
+Route::get('/', 'PagesController@index');
 
-Route::get('about', 'pages\PagesController@about');
+Route::get('about', 'PagesController@about');
 
-Route::get('contact', 'pages\PagesController@contact');
+Route::get('contact', 'PagesController@contact');
