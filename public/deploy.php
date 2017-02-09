016499f8ec654b4634c225e6806232c76aecea31
Ethan Hu <ethan.hqc@icloud.com>
2017-02-09 17:19:17 +0800

    cancel auto deploy

--- public/deploy.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "running on".shell_exec("whoami");
-
-$www_folder = "/var/www/html/testing/" ;
-
-//执行指令 
-echo shell_exec("cd ".$www_folder);
-echo shell_exec("git pull origin master");
